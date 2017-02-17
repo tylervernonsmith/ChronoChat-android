9296c0f6a7d098725913f1c3ef341a26de1596fb
Tyler Smith <tylervernonsmith@users.noreply.github.com>
2017-02-16 23:33:31 -0800

    refactoring: move PingService stuff to NDNService

--- app/src/main/java/edu/ucla/cs/chronochat/NDNService.java
+++ app/src/main/java/edu/ucla/cs/chronochat/NDNService.java
@@ -6,17 +6,24 @@ import android.os.Binder;
 import android.os.IBinder;
 import android.util.Log;
 
+import net.named_data.jndn.Data;
 import net.named_data.jndn.Face;
+import net.named_data.jndn.Interest;
+import net.named_data.jndn.Name;
+import net.named_data.jndn.OnData;
+import net.named_data.jndn.OnTimeout;
 import net.named_data.jndn.encoding.EncodingException;
 
 import java.io.IOException;
 
 public class NDNService extends Service {
 
-    private final Face localhost = new Face("localhost");
-    private boolean destroyed = false;
+    private static final String HOST = "localhost";
+    private static final String NAME_URI = "/localhost/nfd/status/general";
+    private static final String TAG = NDNService.class.getSimpleName();
 
-    private final String TAG = NDNService.class.getSimpleName();
+    private static final Face FACE = new Face(HOST);
+    private static final Name NAME = new Name(NAME_URI);    private boolean destroyed = false;
 
     // Thread for processing events on the face
     private final Thread processEventsThread = new Thread(new Runnable() {
@@ -25,7 +32,7 @@ public class NDNService extends Service {
             Log.d(TAG, "started processEventsThread");
             while (!destroyed) {
                 try {
-                    localhost.processEvents();
+                    FACE.processEvents();
                 } catch (IOException | EncodingException e) {
                     e.printStackTrace();
                 }
@@ -36,6 +43,37 @@ public class NDNService extends Service {
             Log.d(TAG, "stopped processEventsThread");
         }
     });
+
+    /* Implements callbacks for Face.expressInterest() */
+    private static final class PingHandler implements OnData, OnTimeout {
+        private final String TAG = NDNService.class.getSimpleName();
+        @Override
+        public void onData(Interest interest, Data data) {
+            // TODO: Broadcast result
+            Log.d(TAG, "response recv'd");
+        }
+        @Override
+        public void onTimeout(Interest interest) {
+            // TODO: Broadcast result
+            Log.d(TAG, "timed out");
+        }
+        public void startPing() {
+            final Thread thread = new Thread(new Runnable() {
+                @Override
+                public void run () {
+                    try {
+                        FACE.expressInterest(NAME, PingHandler.this, PingHandler.this);
+                    } catch (Exception e) {
+                        // TODO: Broadcast result
+                        Log.d(TAG, "Exception: " + e.toString());
+                    }
+                }
+            });
+            thread.start();
+        }
+    }
+
+    private static final PingHandler handler = new PingHandler();
 
     @Override
     public void onCreate() {
@@ -51,6 +89,12 @@ public class NDNService extends Service {
         destroyed = true; // kills processEventsThread
     }
 
-    /* Basic NDN "services" provided to clients of NDNService */
-    public Face getLocalhost() { return localhost; }
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        handler.startPing();
+        return START_STICKY;
+    }
+
+//    /* Basic NDN "services" provided to clients of NDNService */
+//    public Face getLocalhost() { return localhost; }
 }
