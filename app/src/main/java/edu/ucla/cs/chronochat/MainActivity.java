d128e5efa9e24d8db4cb957137f2576567f73674
Tyler Smith <tylervernonsmith@users.noreply.github.com>
2017-02-16 21:23:35 -0800

    partial revert, bind NDNService in onCreate

--- app/src/main/java/edu/ucla/cs/chronochat/MainActivity.java
+++ app/src/main/java/edu/ucla/cs/chronochat/MainActivity.java
@@ -1,23 +1,51 @@
 package edu.ucla.cs.chronochat;
 
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.IBinder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    NDNService service;
+    boolean serviceBound = false;
+
+    private ServiceConnection serviceConn = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder binder) {
+            NDNService.NDNBinder ndnBinder = (NDNService.NDNBinder) binder;
+            service = ndnBinder.getService();
+            serviceBound = true;
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            serviceBound = false;
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Intent intent = new Intent(this, NDNService.class);
+        bindService(intent, serviceConn, Context.BIND_AUTO_CREATE);
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        Intent intent = new Intent(this, PingService.class);
-        this.startService(intent);
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        unbindService(serviceConn);
     }
 
 }
+
