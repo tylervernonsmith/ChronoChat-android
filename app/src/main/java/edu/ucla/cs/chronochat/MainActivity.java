45859f7c208403767e29a9ccc4ec10c5a3d83ac0
Tyler Smith <tylervernonsmith@users.noreply.github.com>
2017-02-16 19:17:01 -0800

    add PingService

--- app/src/main/java/edu/ucla/cs/chronochat/MainActivity.java
+++ app/src/main/java/edu/ucla/cs/chronochat/MainActivity.java
@@ -1,31 +1,27 @@
 package edu.ucla.cs.chronochat;
 
-import android.content.ComponentName;
-import android.content.Context;
 import android.content.Intent;
-import android.content.ServiceConnection;
-import android.os.IBinder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-    NDNService service;
-    boolean serviceBound = false;
-
-    private ServiceConnection serviceConn = new ServiceConnection() {
-        @Override
-        public void onServiceConnected(ComponentName name, IBinder binder) {
-            NDNService.NDNBinder ndnBinder = (NDNService.NDNBinder) binder;
-            service = ndnBinder.getService();
-            serviceBound = true;
-        }
-
-        @Override
-        public void onServiceDisconnected(ComponentName name) {
-            serviceBound = false;
-        }
-    };
+//    NDNService service;
+//    boolean serviceBound = false;
+//
+//    private ServiceConnection serviceConn = new ServiceConnection() {
+//        @Override
+//        public void onServiceConnected(ComponentName name, IBinder binder) {
+//            NDNService.NDNBinder ndnBinder = (NDNService.NDNBinder) binder;
+//            service = ndnBinder.getService();
+//            serviceBound = true;
+//        }
+//
+//        @Override
+//        public void onServiceDisconnected(ComponentName name) {
+//            serviceBound = false;
+//        }
+//    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,17 +32,19 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onStart() {
         super.onStart();
-        // Bind to NDNService
-        Intent intent = new Intent(this, NDNService.class);
-        bindService(intent, serviceConn, Context.BIND_AUTO_CREATE);
+//        // Bind to NDNService
+//        Intent intent = new Intent(this, NDNService.class);
+//        bindService(intent, serviceConn, Context.BIND_AUTO_CREATE);
+        Intent intent = new Intent(this, PingService.class);
+        this.startService(intent);
     }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        if (serviceBound) {
-            unbindService(serviceConn);
-            serviceBound = false;
-        }
-    }
+//
+//    @Override
+//    protected void onStop() {
+//        super.onStop();
+//        if (serviceBound) {
+//            unbindService(serviceConn);
+//            serviceBound = false;
+//        }
+//    }
 }
